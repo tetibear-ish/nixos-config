afcbbf0f5ead308902f96044fa0a1411ec819b38
tetibear-ish <tetibear@a2z-technologies.com>
2026-09-17 22:45:40 -0400

    Fix permissions for tmux plugins folder

--- configuration.nix
+++ configuration.nix
@@ -158,9 +158,9 @@
   };
 
   system.activationScripts.tpm.text = ''
+    mkdir -p /home/tetibear/.tmux/plugins
+    chown tetibear:users /home/tetibear/.tmux /home/tetibear/.tmux/plugins
     if [ ! -d /home/tetibear/.tmux/plugins/tpm ]; then
-      mkdir -p /home/tetibear/.tmux/plugins
-      chown tetibear:users /home/tetibear/.tmux /home/tetibear/.tmux/plugins
       ${pkgs.git}/bin/git clone https://github.com/tmux-plugins/tpm /home/tetibear/.tmux/plugins/tpm
       chown -R tetibear:users /home/tetibear/.tmux/plugins/tpm
     fi
