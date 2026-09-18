72a71c435f320dbb83dec08f625a47b3d4de8ec5
tetibear-ish <tetibear@a2z-technologies.com>
2026-09-17 22:53:58 -0400

    Fix tmux permissions

--- configuration.nix
+++ configuration.nix
@@ -159,11 +159,10 @@
 
   system.activationScripts.tpm.text = ''
     mkdir -p /home/tetibear/.tmux/plugins
-    chown tetibear:users /home/tetibear/.tmux /home/tetibear/.tmux/plugins
     if [ ! -d /home/tetibear/.tmux/plugins/tpm ]; then
       ${pkgs.git}/bin/git clone https://github.com/tmux-plugins/tpm /home/tetibear/.tmux/plugins/tpm
-      chown -R tetibear:users /home/tetibear/.tmux/plugins/tpm
     fi
+    chown -R tetibear:users /home/tetibear/.tmux
   '';
 
   # Some programs need SUID wrappers, can be configured further or are
